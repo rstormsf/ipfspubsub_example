12af2d663a6a1d5e8ea2a3cd320287cedecca2dd
Roman Storm <rstormsf@gmail.com>
2018-10-08 23:20:12 -0700

    Updates

---
+++ precache-manifest.22e2429ae1ae371d0977a608b86b1237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ipfspubsub/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e58763517b5e69da4cd6",
+    "url": "/ipfspubsub/static/js/runtime~main.e5876351.js"
+  },
+  {
+    "revision": "1edcae49339d0c048bfd",
+    "url": "/ipfspubsub/static/js/main.1edcae49.chunk.js"
+  },
+  {
+    "revision": "25eefb3ad461d0057f01",
+    "url": "/ipfspubsub/static/js/1.25eefb3a.chunk.js"
+  },
+  {
+    "revision": "1edcae49339d0c048bfd",
+    "url": "/ipfspubsub/static/css/main.c6bd2984.chunk.css"
+  },
+  {
+    "revision": "28d395d90e94f07a5fb1e4d69b3bca73",
+    "url": "/ipfspubsub/index.html"
+  }
+];
